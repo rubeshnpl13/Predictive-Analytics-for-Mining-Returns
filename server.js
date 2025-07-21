84832de8611740e323947dfec01d4fee5e3e62d4
nishant <nepalnishanta29@gmail.com>
2025-07-21 13:01:39 +0545

    Added API KEY(from etherscan) and block numbers and reward

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const axios = require('axios');
+require('dotenv').config();
 
 const app = express();
+const apiKey = process.env.APT_KEY;
 
 class Block {
   constructor(timeStamp, blockReward) {
@@ -9,6 +11,21 @@ class Block {
     this.blockReward = blockReward;
   }
 }
+
+const fetchData = async () => {
+  try {
+    const lstOfBlocks = [];
+    for (let blockNumber = 1746572; blockNumber < 17465723; blockNumber++) {
+      const apiUrl = `https://api.etherscan.io/v2/api
+                        &module=block
+                        &action=getblockreward
+                        &blockno=${blockNumber}
+                        &apikey=${apiKey}`;
+    }
+  } catch (erro) {
+    console.log(error);
+  }
+};
 
 app.listen(3000, () => {
   console.log('Server is running');
