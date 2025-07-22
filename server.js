a77c38a15902e1c8b4c38b878ffaf78d5bf6281a
nishant <nepalnishanta29@gmail.com>
2025-07-22 11:41:09 +0545

    Blockreward conversion to ether value

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const axios = require('axios');
+const { utils } = require('ethers');
 require('dotenv').config();
+//console.log('API KEY', apiKey);
 
 const app = express();
-const apiKey = process.env.APT_KEY;
+const apiKey = process.env.API_KEY;
 
 class Block {
   constructor(timeStamp, blockReward) {
@@ -15,18 +17,24 @@ class Block {
 const fetchData = async () => {
   try {
     const lstOfBlocks = [];
-    for (let blockNumber = 1746572; blockNumber < 17465723; blockNumber++) {
-      const apiUrl = `https://api.etherscan.io/v2/api
-                        &module=block
-                        &action=getblockreward
-                        &blockno=${blockNumber}
-                        &apikey=${apiKey}`;
+    for (let blockNumber = 1746572; blockNumber < 1746573; blockNumber++) {
+      const apiUrl = `https://api.etherscan.io/v2/api?chainid=1&module=block&action=getblockreward&blockno=${blockNumber}&apikey=${apiKey}`;
+
+      const response = await axios.get(apiUrl);
+      const rewardEther = utils.formatEther(response.data.result.blockReward);
+      const timeStamp = response.data.result.timeStamp;
+      const block = new Block(timeStamp, rewardEther);
+      console.log(block);
+      // listofBlocks.push(block);
+      //   console.log(response.data.result.blockReward);
+      //   console.log(response.data.result.timeStamp);
     }
-  } catch (erro) {
+  } catch (error) {
     console.log(error);
   }
 };
-
+//console.log('API KEY', apiKey);
+fetchData();
 app.listen(3000, () => {
   console.log('Server is running');
 });
